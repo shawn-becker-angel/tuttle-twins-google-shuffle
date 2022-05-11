f23a180730aa47e2ca5746afa8cd1877cd16fa7c
Shawn Becker <shawn.becker@angel.com>
2022-05-11 10:38:56 -0600

    fixed last_modified bug

--- s3_key.py
+++ s3_key.py
@@ -8,27 +8,26 @@ class s3_key:
     key: str
     
     def __init__(self, s3_ls_line: str):
-            '''
-            Take one line of the result of 'aws s3 ls --recursive <path>', e.g.
-            "2022-05-03 19:15:44       2336 tuttle_twins/ML/validate/Uncommon/TT_S01_E01_FRM-00-19-16-19.jpg\n"
-            remove the trailing "\n
-            and parse it into a s3_key dict, e.g.
-            {"last_modified": 2022-05-03T19:15:44 , "size":2336, "key":"tuttle_twins/ML/validate/Uncommon/TT_S01_E01_FRM-00-19-16-19.jpg"}
-            '''
-            try:
-                # replace multi-spaces with one
-                s3_ls_line = ' '.join(s3_ls_line.split())
-                
-                # remove trailing newline
-                s3_ls_line = s3_ls_line.replace("\n", "")
-                
-                parts = s3_ls_line.split(" ")
-                self.last_modified = datetime.datetime((parts[0] + "T" + parts[1]), '%Y-%m-%dT%H:%M:%S') 
-                self.size = parts[2]
-                self.key = parts[3]
-            except Exception as exp:
-                print(type(exp),str(exp))
-                raise
+        '''
+        Take one line of the result of 'aws s3 ls --recursive <path>', e.g.
+        "2022-05-03 19:15:44       2336 tuttle_twins/ML/validate/Uncommon/TT_S01_E01_FRM-00-19-16-19.jpg\n"
+        remove the trailing "\n
+        and parse it into a s3_key dict, e.g.
+        {"last_modified": 2022-05-03T19:15:44 , "size":2336, "key":"tuttle_twins/ML/validate/Uncommon/TT_S01_E01_FRM-00-19-16-19.jpg"}
+        '''
+        try:
+            # replace multi-spaces with one
+            s3_ls_line = ' '.join(s3_ls_line.split())
+            # remove trailing newline
+            s3_ls_line = s3_ls_line.replace("\n", "")
+            parts = s3_ls_line.split(" ")
+            dt_str = parts[0] + "T" + parts[1]
+            self.last_modified = datetime.datetime.strptime(parts[0] + "T" + parts[1], "%Y-%m-%dT%H:%M:%S")
+            self.size = parts[2]
+            self.key = parts[3]
+        except Exception as exp:
+            print(type(exp),str(exp))
+            raise
 
 
     def as_dict(self):
