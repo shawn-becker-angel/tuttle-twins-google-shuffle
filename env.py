39529a3bca702bbadd616954a61840b747454c11
Shawn Becker <shawn.becker@angel.com>
2022-05-24 11:59:42 -0600

    env.py asserts existance of files and dirs

--- env.py
+++ env.py
@@ -12,6 +12,7 @@ load_dotenv()
 # Data team.
 # See the README.md file for instructions
 GOOGLE_CREDENTIALS_FILE = os.getenv("GOOGLE_CREDENTIALS_FILE")
+assert os.path.isfile(GOOGLE_CREDENTIALS_FILE)
 
 # The main S3 bucket
 S3_MEDIA_ANGEL_NFT_BUCKET = os.getenv("S3_MEDIA_ANGEL_NFT_BUCKET")
@@ -23,6 +24,8 @@ S3_MANIFESTS_DIR = os.getenv("S3_MANIFESTS_DIR")
 # Local directory for the stage data files
 # e.g. train.csv,  test.csv,  pred.csv
 LOCAL_DATA_FILES_DIR = os.getenv("LOCAL_DATA_FILES_DIR")
+assert os.path.isdir(LOCAL_DATA_FILES_DIR)
 
 # Directory for local copies of all source image files synced from S3
 LOCAL_SOURCE_IMAGES_DIR = os.getenv("LOCAL_SOURCE_IMAGES_DIR")
+assert os.path.isdir(LOCAL_SOURCE_IMAGES_DIR)
